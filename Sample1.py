6599300f653e2dd6d8d1219754b8291de267581e
VENKATESH T <venkatesh.usha6041@gmail.com>
2022-12-15 13:56:11 +0530

    update venkat_v1

--- Sample1.py
+++ Sample1.py
@@ -1,29 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-from selenium.webdriver.support.wait import WebDriverWait
-
-s=Service(executable_path="C:\\Selenium Web Drivers\\chromedriver_win32\\chromedriver.exe")
-driver=webdriver.Chrome(service=s)
-driver.get("http://www.fb.com")
-driver.maximize_window()
-
-driver.find_element(By.XPATH,"//a[contains(@id,'u_0_0_')]").click()
-WebDriverWait(driver,10).until(EC.presence_of_element_located((By.NAME,"firstname"))).send_keys("Rajesh")
-driver.find_element(By.NAME,"lastname").send_keys("Kanna")
-driver.find_element(By.NAME,"reg_email__").send_keys("rajesh@gmail.com")
-
 def add(a,b):
-  c=a+b
-  print c
-  
-def sub(a,b):
+  c = a+b
+  print(c)
+
+def  sub(a,b):
   c=a-b
-  print c
-  
-def mul(a,b):
-  c=a*b
-  print c
+  print(c)
+
 
